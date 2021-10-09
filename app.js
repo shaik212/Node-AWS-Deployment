2843233799e445919cf35bbf555c9a9f69f0b7c6
shaik212 <80620740+shaik212@users.noreply.github.com>
2021-10-09 15:26:46 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.get("/", function (req, res) {
 app.get("/new", function (req, res) {
   res.send("New also worked");
 });
+app.get("/update", function (req, res) {
+  res.send("New update also worked");
+});
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
